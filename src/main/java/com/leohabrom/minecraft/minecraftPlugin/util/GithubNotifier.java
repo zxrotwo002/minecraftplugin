a7675c867b74bd3eb101caf9a6bebd22ff8bebf6
Leo <leonard.habrom@uni-ulm.de>
2025-08-30 18:08:51 +0200

    debug: added debug stuff

--- src/main/java/com/leohabrom/minecraft/minecraftPlugin/util/GithubNotifier.java
+++ src/main/java/com/leohabrom/minecraft/minecraftPlugin/util/GithubNotifier.java
@@ -11,9 +11,11 @@ import java.util.logging.Level;
 public class GithubNotifier implements Runnable {
     InputStream inputStream;
     public GithubNotifier() {
+        Bukkit.broadcast(Component.text("Notifier"));
         try {
             Socket socket = new Socket("creative.leohabrom.com",25065);
             inputStream = socket.getInputStream();
+            Bukkit.broadcast(Component.text("IS"));
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
@@ -23,10 +25,12 @@ public class GithubNotifier implements Runnable {
     @Override
     public void run() {
         if (inputStream == null) {
+            Bukkit.broadcast(Component.text("IS null"));
             MinecraftPlugin.getInstance().getLogger().log(Level.INFO,"Couldn't open Github Notifier Stream");
             return;
         }
         try {
+            Bukkit.broadcast(Component.text("try"));
             BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
             String line;
             while ((line = reader.readLine())!=null) {
