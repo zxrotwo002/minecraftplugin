3fa78c334343b63a4403f1ee2c6e3b242363deba
Leo <leonard.habrom@uni-ulm.de>
2025-08-30 18:18:41 +0200

    debug: broadcast

--- src/main/java/com/leohabrom/minecraft/minecraftPlugin/MinecraftPlugin.java
+++ src/main/java/com/leohabrom/minecraft/minecraftPlugin/MinecraftPlugin.java
@@ -1,6 +1,7 @@
 package com.leohabrom.minecraft.minecraftPlugin;
 
 import com.leohabrom.minecraft.minecraftPlugin.util.GithubNotifier;
+import net.kyori.adventure.text.Component;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.logging.Level;
@@ -18,6 +19,7 @@ public final class MinecraftPlugin extends JavaPlugin {
         instance = this;
         // Plugin startup logic
         getLogger().log(Level.INFO,"Hello from MinecraftPlugin");
+        getServer().broadcast(Component.text("Test"));
         new GithubNotifier();
     }
 
