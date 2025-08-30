e3ce47850075248f3164c6f1d44c240947a5d447
Leo <leonard.habrom@uni-ulm.de>
2025-08-30 18:03:08 +0200

    feat: Broadcast notification from echoserver

--- src/main/java/com/leohabrom/minecraft/minecraftPlugin/MinecraftPlugin.java
+++ src/main/java/com/leohabrom/minecraft/minecraftPlugin/MinecraftPlugin.java
@@ -6,14 +6,23 @@ import java.util.logging.Level;
 
 public final class MinecraftPlugin extends JavaPlugin {
 
+    public static MinecraftPlugin instance;
+
+    public static MinecraftPlugin getInstance() {
+        return instance;
+    }
+
     @Override
     public void onEnable() {
+        instance = this;
         // Plugin startup logic
         getLogger().log(Level.INFO,"Hello from MinecraftPlugin");
+
     }
 
     @Override
     public void onDisable() {
+        instance = null;
         // Plugin shutdown logic
     }
 }
---
+++ src/main/java/com/leohabrom/minecraft/minecraftPlugin/util/GithubNotifier.java
@@ -0,0 +1,39 @@
+package com.leohabrom.minecraft.minecraftPlugin.util;
+
+import com.leohabrom.minecraft.minecraftPlugin.MinecraftPlugin;
+import net.kyori.adventure.text.Component;
+import org.bukkit.Bukkit;
+
+import java.io.*;
+import java.net.Socket;
+import java.util.logging.Level;
+
+public class GithubNotifier implements Runnable {
+    InputStream inputStream;
+    public GithubNotifier() {
+        try {
+            Socket socket = new Socket("creative.leohabrom.com",25065);
+            inputStream = socket.getInputStream();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        new Thread(this).start();
+    }
+
+    @Override
+    public void run() {
+        if (inputStream == null) {
+            MinecraftPlugin.getInstance().getLogger().log(Level.INFO,"Couldn't open Github Notifier Stream");
+            return;
+        }
+        try {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+            String line;
+            while ((line = reader.readLine())!=null) {
+                Bukkit.broadcast(Component.text(line));
+            }
+        } catch (IOException e) {
+            MinecraftPlugin.getInstance().getLogger().log(Level.WARNING,e.getMessage(),e.getCause());
+        }
+    }
+}
